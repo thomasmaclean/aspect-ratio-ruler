ff025274ea5a6e54ac6ef722e200929819601d66
thomasmaclean <thomas.maclean@nbcuni.com>
2016-03-31 11:44:10 -0400

    Fixed icon behavior.

--- background.js
+++ background.js
@@ -1,16 +1,21 @@
-var toggle = false;
-function toggleRuler() {
-  toggle = !toggle;
-  if(toggle){
-    chrome.browserAction.setBadgeText({text: 'ready'});
-    chrome.browserAction.setBadgeBackgroundColor({color:[0,0,0,128]});
+var toggle = [];
+
+chrome.browserAction.setIcon({path: 'off.png'});
+
+function toggleRuler(tab) {
+  toggle[tab.id] = typeof toggle[tab.id] === 'undefined' ? true : !toggle[tab.id];
+  if(toggle[tab.id]){
+    chrome.browserAction.setIcon({path: 'on.png', tabId:tab.id});
+	  //chrome.browserAction.setBadgeText({text: 'ready'});
+    //chrome.browserAction.setBadgeBackgroundColor({color:[0,0,0,128]});
     chrome.tabs.executeScript({file:"ruler.js"});
     chrome.tabs.insertCSS({file:"ruler.css"});
   }
   else{
-    chrome.browserAction.setBadgeText({text: ''});
+    chrome.browserAction.setIcon({path: 'off.png', tabId:tab.id});
+    //chrome.browserAction.setBadgeText({text: ''});
     chrome.tabs.insertCSS({code:""});
-    chrome.tabs.executeScript({code:"document.body.removeChild(document.getElementById('ar-select-board'));"});
+    chrome.tabs.executeScript({code:"(c = document.getElementById('ar-select-board')) ? document.body.removeChild(c) : null;"});
   }
 }
 chrome.browserAction.onClicked.addListener(toggleRuler);
